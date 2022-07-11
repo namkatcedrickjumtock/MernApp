c7967f152ef5558cdbd3a1b8be30206c5d4d03ba
Namkat Cedrick j <namkatcedrickjumtock3@gmail.com>
2022-07-11 09:25:51 +0100

    initial comite

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+
+// components
+import WorkoutDetails from "../components/workDetails";
+import WorkOutForm from "../components/WorkOutForm";
+
+const Home = () => {
+  const [workouts, setWorkouts] = useState(null);
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("/api/workouts");
+      const json = await response.json();
+
+      if (response.ok) {
+        setWorkouts(json);
+      }
+      
+    };
+
+    fetchWorkouts();
+  }, []);
+
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutDetails workout={workout} key={workout._id} />
+          ))}
+      </div>
+      <WorkOutForm />
+    </div>
+  );
+};
+
+export default Home;
