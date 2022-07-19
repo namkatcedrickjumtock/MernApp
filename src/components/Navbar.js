95da2014db969a0790db29640df756e0fdaceff7
Namkat Cedrick j <namkatcedrickjumtock3@gmail.com>
2022-07-20 00:24:43 +0100

    code clean up in build

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,7 @@ const Navbar = () => {
     <header>
       <div className="container">
         <Link to="/">
+          <img src="/icon/workLogo.png" alt="logo"  width={90}/>
           <h1>Workout Buddy</h1>
         </Link>
       </div>
